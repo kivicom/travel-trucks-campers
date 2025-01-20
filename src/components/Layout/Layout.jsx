13c4aa23fcfa7de3dbcc37cd9279244b9c9c2cda
Igor <aigor9534@gmail.com>
2025-01-20 15:12:46 +0200

    Remove Suspense, Loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,10 @@
-import { Suspense } from 'react';
-import Loader from '../Loader/Loader';
 import Header from '../Header/Header';
 
 export const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <Suspense fallback={<Loader />}> {children} </Suspense>
+      {children}
     </>
   );
 };
