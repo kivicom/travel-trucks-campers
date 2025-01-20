185395f103fdf59cb1505bfd0369b97f352c7454
Igor <aigor9534@gmail.com>
2025-01-20 15:12:08 +0200

    Add useffect, dispatch

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,9 +1,18 @@
+import { useDispatch } from 'react-redux';
 import CampersList from '../../components/CampersList/CampersList';
 import Container from '../../components/Container/Container';
 import SideBar from '../../components/SideBar/SideBar';
 import styles from './CatalogPage.module.css';
+import { useEffect } from 'react';
+import { fetchCampers } from '../../redux/campers/operations';
 
 const CatalogPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampers());
+  }, [dispatch]);
+
   return (
     <section>
       <Container>
